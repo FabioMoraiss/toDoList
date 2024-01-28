e8057d7c8d701ec5dfa31d0f1861f315203c5852
Fabio Morais <rarinaldo@outlook.com>
2024-01-28 19:14:34 -0300

    adicionando teste automatizados

--- challenge-todolist/src/main/java/br/com/fabiomorais/challengetodolist/entity/Todo.java
+++ challenge-todolist/src/main/java/br/com/fabiomorais/challengetodolist/entity/Todo.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "todos")
@@ -12,10 +13,21 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
     private boolean finished;
     private int priority;
+    
+
+    
+    public Todo(String name, String description, boolean finished, int priority) {
+        this.name = name;
+        this.description = description;
+        this.finished = finished;
+        this.priority = priority;
+    }
     
     public long getId() {
         return id;
--- challenge-todolist/src/test/java/br/com/fabiomorais/challengetodolist/ChallengeTodolistApplicationTests.java
+++ challenge-todolist/src/test/java/br/com/fabiomorais/challengetodolist/ChallengeTodolistApplicationTests.java
@@ -1,13 +1,48 @@
 package br.com.fabiomorais.challengetodolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import br.com.fabiomorais.challengetodolist.entity.Todo;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ChallengeTodolistApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testeCreateTodoSucess() {
+		var todo = new Todo("todo teste 1", "descricao teste 1", false, 1);
+
+		webTestClient
+		.post().uri("/todos")
+		.bodyValue(todo)
+		.exchange()
+		.expectStatus().isOk()
+		.expectBody()
+		.jsonPath("$").isArray()
+		.jsonPath("$.length()").isEqualTo(1)
+		.jsonPath("$[0].name").isEqualTo(todo.getName())
+		.jsonPath("$[0].description").isEqualTo(todo.getDescription())
+		.jsonPath("$[0].finished").isEqualTo(todo.isFinished())
+		.jsonPath("$[0].priority").isEqualTo(todo.getPriority());
+
+	}
+	@Test
+	void testeCreateTodoFailure() {
+		var todo = new Todo("todo teste 1", "descricao teste 1", false, 1);
+
+		webTestClient
+		.post()
+		.uri("/todos")
+		.bodyValue(todo)
+		.exchange()
+		.expectStatus().isBadRequest();
+		
 	}
 
 }
